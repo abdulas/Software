ce13405786898058a95006a77a8981239a31fa40
Abdullah Ayman Salha <112077676+abdulas@users.noreply.github.com>
2022-10-26 15:23:25 +0200

    Add files via upload

---
+++ src/test/java/yearloop_Test.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class yearloop_Test{
+
+    //Når det er delelig med 4, men ikke 100
+    @Test
+    public void when_divided_by_4_not_100() {assertEquals( true, Yearloop.isLeapYear(1916));}
+
+    //Når det er delelig med 400
+    @Test
+    public void when_divided_by_400_() {assertEquals(true, Yearloop.isLeapYear(2000));
+    }
+
+    //Når det ikke er delelig med 4
+    @Test
+    public void not_divides_by_4() {assertEquals(false, Yearloop.isLeapYear(1800));
+    }
+
+    @Test
+    public void not_divides_by_400() {assertEquals(false, Yearloop.isLeapYear(2100));
+    }
+
+}
